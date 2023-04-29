678624a1234e25cf7bd12affb0553065bf037a9e
eihap <eihapinfo@gmail.com>
2023-04-29 13:06:53 +0300

    create BankAccount domain

---
+++ Domains/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains
+{
+    public class BankAccount
+    {
+        [Key]
+        [ValidateNever]
+        public int BankAccountId { get; set; }
+
+        [ValidateNever]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Account number is required.")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Account number must be between 6 and 20 characters.")]
+        public string AccountNumber { get; set; }
+
+        [Required(ErrorMessage = "Bank name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Bank name must be between 2 and 50 characters.")]
+        public string BankName { get; set; }
+
+        [Required(ErrorMessage = "Branch number is required.")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Branch number must be between 2 and 20 characters.")]
+        public string BranchNumber { get; set; }
+
+        [Required]
+        [Range(0.000000000001, 9999999999.99, ErrorMessage = "Balance must be between 0.000000000001 and 9999999999.99.")]
+        public decimal Balance { get; set; }
+
+        public virtual User User { get; set; }
+    }
+
+}
