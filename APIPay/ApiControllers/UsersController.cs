90934213976b41b54174883b04cc72b472a1ebd9
eihap <eihapinfo@gmail.com>
2023-04-29 17:43:04 +0300

    Create dependency injection region for Api controller

---
+++ APIPay/ApiControllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Domains;
+using Microsoft.AspNetCore.Mvc;
+using static Bl.IBusinessLayer;
+
+namespace APIPay.ApiControllers
+{
+    public class UsersController : Controller
+    {
+        #region dependency injection region
+        IBusinessLayer<TbUser> oClsUsers;
+        public UsersController(IBusinessLayer<TbUser> Users, IBusinessLayer<TbFinancialTransaction> financialTransaction, IBusinessLayer<VwTransWacountDetail> vwTransWacountDetails)
+        {
+            oClsUsers = Users;
+            
+        }
+        #endregion
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
