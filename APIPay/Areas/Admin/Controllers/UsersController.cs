5b6fa70eb6710f7ce9956397d225083aa93a940b
eihap <eihapinfo@gmail.com>
2023-04-29 16:16:18 +0300

    Creat GetAll users function-controller

--- APIPay/Areas/Admin/Controllers/UsersController.cs
+++ APIPay/Areas/Admin/Controllers/UsersController.cs
@@ -12,11 +12,22 @@ namespace APIPay.Areas.Admin.Controllers
         public UsersController(IBusinessLayer<TbUser> Users)
         {
             oClsUsers = Users;
-        } 
+        }
         #endregion
+
+        #region Index
         public IActionResult Index()
         {
             return View();
+        } 
+        #endregion
+
+        #region List of Users
+        public IActionResult List()
+        {
+            var lstUsers = oClsUsers.GetAll();
+            return View(lstUsers);
         }
+        #endregion
     }
 }
