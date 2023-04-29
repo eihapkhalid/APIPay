745a70979e7ef003b3fe23f24535bf3707918ed1
eihap <eihapinfo@gmail.com>
2023-04-29 15:38:30 +0300

    define DbContext in ClsUsers Class

---
+++ Bl/ClsUsers.cs
@@ -0,0 +1,51 @@
+﻿using Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Bl.IBusinessLayer;
+
+namespace Bl
+{
+    public class ClsUsers : IBusinessLayer<TbUser>
+    {
+        #region define DbContext
+        PaymentUserDbContext context;
+        public ClsUsers(PaymentUserDbContext ctx)
+        {
+            context = ctx;
+        } 
+        #endregion
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(TbUser table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TbUser> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TbUser GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Payments(TbUser table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(TbUser table)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
