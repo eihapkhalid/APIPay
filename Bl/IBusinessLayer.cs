b8ea943e15d5ddc8cbda1acbadde8cb11a7ed4b6
eihap <eihapinfo@gmail.com>
2023-04-29 15:32:27 +0300

    creat an intrface (IBusinessLayer)

---
+++ Bl/IBusinessLayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl
+{
+    public interface IBusinessLayer
+    {
+        public interface IBusinessLayer<t>
+        {
+            List<t> GetAll();
+            t GetById(int id);
+            bool Save(t table);
+            bool Delete(int id);
+            bool Delete(t table);
+            bool Payments(t table);
+        }
+    }
+}
