d39a7494088da01c729f7ab71a962ca7959b0bf1
eihap <eihapinfo@gmail.com>
2023-04-29 16:14:35 +0300

    create usersControler with depndency injection from clsTbusers

---
+++ APIPay/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Domains;
+using Microsoft.AspNetCore.Mvc;
+using static Bl.IBusinessLayer;
+
+namespace APIPay.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        #region Dependancy Injections
+        IBusinessLayer<TbUser> oClsUsers;
+        public UsersController(IBusinessLayer<TbUser> Users)
+        {
+            oClsUsers = Users;
+        } 
+        #endregion
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
