6084054c4fb7f1378066f1728c6b4f801643a28b
eihap <eihapinfo@gmail.com>
2023-04-30 11:32:18 +0300

    create dependency injection for TbPayment into PaymentsController

--- APIPay/ApiControllers/PaymentsController.cs
+++ APIPay/ApiControllers/PaymentsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domains;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static Bl.IBusinessLayer;
 
 namespace APIPay.ApiControllers
 {
@@ -7,5 +9,13 @@ namespace APIPay.ApiControllers
     [ApiController]
     public class PaymentsController : ControllerBase
     {
+        #region dependency injection region
+        IBusinessLayer<TbPayment> oClsTbPayment;
+        public PaymentsController(IBusinessLayer<TbPayment> payment)
+        {
+            oClsTbPayment = payment;
+
+        }
+        #endregion
     }
 }
