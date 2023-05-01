35130e05052e5e2948a36859964d6632a91c53a5
eihap <eihapinfo@gmail.com>
2023-05-01 11:56:19 +0300

    GetBankAccountById Test

--- UnitTests/BankAccountTests.cs
+++ UnitTests/BankAccountTests.cs
@@ -30,19 +30,35 @@ namespace UnitTests
         public void GetAllBankAccounts_ReturnsListOfBankAccounts()
         {
             // Arrange
-            var expectedUsers = new List<TbBankAccount>
+            var expectedBankAccounts = new List<TbBankAccount>
                     {
-                        new TbBankAccount { UserId = 1, AccountNumber = "1234567891011121344", BankName="bank1", BranchNumber="Sudan", Balance =(decimal)200.25, CurrentState = 1 },
-                        new TbBankAccount {  UserId = 1, AccountNumber ="9867510531011121344", BankName="bank2", BranchNumber="Saudi", Balance =(decimal)10.25F, CurrentState = 1 }
+                        new TbBankAccount {  UserId = 1, AccountNumber = "1234567891011121344", BankName="bank1", BranchNumber="Sudan", Balance =(decimal)200.25, CurrentState = 1 },
+                        new TbBankAccount {  UserId = 1, AccountNumber = "9867510531011121344", BankName="bank2", BranchNumber="Saudi", Balance =(decimal)10.25F, CurrentState = 1 }
                     };
-            _mockBankAccountBusinessLayer.Setup(b => b.GetAll()).Returns(expectedUsers);
+            _mockBankAccountBusinessLayer.Setup(b => b.GetAll()).Returns(expectedBankAccounts);
             // Act
             var result = _controller.Get();
 
             // Assert
-            Assert.AreEqual(expectedUsers, result);
+            Assert.AreEqual(expectedBankAccounts, result);
         }
         #endregion
 
+        [TestMethod]
+        #region GetBankAccountById
+        public void GetBankAccountById_ReturnsUser()
+        {
+            // Arrange
+            var userId = 1;
+            var expectedBankAccount = new TbBankAccount { UserId = 1, AccountNumber = "1234567891011121344", BankName = "bank1", BranchNumber = "Sudan", Balance = (decimal)200.25, CurrentState = 1 };
+            _mockBankAccountBusinessLayer.Setup(b => b.GetById(userId)).Returns(expectedBankAccount);
+
+            // Act
+            var result = _controller.Get(userId);
+
+            // Assert
+            Assert.AreEqual(expectedBankAccount, result);
+        }
+        #endregion
     }
 }
