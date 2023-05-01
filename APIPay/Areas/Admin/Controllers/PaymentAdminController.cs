fc77192aff940e32b99de55b41c5bc90081f5050
eihap <eihapinfo@gmail.com>
2023-05-01 16:47:49 +0300

    update

--- APIPay/Areas/Admin/Controllers/PaymentAdminController.cs
+++ APIPay/Areas/Admin/Controllers/PaymentAdminController.cs
@@ -1,13 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bl;
+using Domains;
+using Microsoft.AspNetCore.Mvc;
+using static Bl.IBusinessLayer;
 
 namespace APIPay.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class PaymentAdminController : Controller
+    public class PaymentsController : Controller
     {
-        public IActionResult Index()
+        #region Dependancy Injections
+        IBusinessLayer<TbPayment> oClsTbPayment;
+        public PaymentsController(IBusinessLayer<TbPayment> payment)
         {
-            return View();
+            oClsTbPayment = payment;
         }
+        #endregion
+
+        #region List of Payments
+        public IActionResult List()
+        {
+            var lstPayments = oClsTbPayment.GetAll();
+            return View(lstPayments);
+        }
+        #endregion
+
+        #region Edit Payment by id
+        public IActionResult Edit(int? paymentId)
+        {
+            var ObjPayment = new TbPayment();
+
+            if (paymentId != null)
+            {
+                ObjPayment = oClsTbPayment.GetById(Convert.ToInt32(paymentId));
+            }
+            return View(ObjPayment);
+        }
+        #endregion
+
+        #region Save by payment object
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(TbPayment payment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Edit", payment);
+            }
+            oClsTbPayment.Save(payment);
+            return RedirectToAction("List");
+        }
+        #endregion
+
+        #region Delete By payment Id
+        public IActionResult Delete(int paymentId)
+        {
+            oClsTbPayment.Delete(paymentId);
+            return RedirectToAction("List");
+        }
+        #endregion
     }
 }
