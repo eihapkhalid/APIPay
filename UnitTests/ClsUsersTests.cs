92d797ab737461e017d9e3de6a4ce45c7e6b76e1
eihap <eihapinfo@gmail.com>
2023-05-01 11:32:15 +0300

    inject (users controller and Bl of users into ClsUsersTests)

--- UnitTests/ClsUsersTests.cs
+++ UnitTests/ClsUsersTests.cs
@@ -1,14 +1,31 @@
-﻿using Bl;
-using Domains;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using APIPay.ApiControllers;
+using Bl;
 using System.Collections.Generic;
-using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Domains;
+using static Bl.IBusinessLayer;
 
 namespace UnitTests
 {
+    [TestClass]
     public class ClsUsersTests
     {
+        #region dependency injection region
+        private readonly UsersController _controller;
+        private readonly Mock<IBusinessLayer<TbUser>> _mockUsersBusinessLayer;
+        
+        public ClsUsersTests()
+        {
+            _mockUsersBusinessLayer = new Mock<IBusinessLayer<TbUser>>();
+            _controller = new UsersController
+                (
+                    _mockUsersBusinessLayer.Object
+                );
+        }
+        #endregion
+
+
     }
 }
