ffc96a7338304499189df52fe553e395b5fac10c
eihap <eihapinfo@gmail.com>
2023-05-01 14:54:08 +0300

    inject clsTbpayment into paymentcontroller

---
+++ APIPay/Areas/Admin/Controllers/PaymentController.cs
@@ -0,0 +1,20 @@
+﻿using Domains;
+using Microsoft.AspNetCore.Mvc;
+using static Bl.IBusinessLayer;
+
+namespace APIPay.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PaymentController : Controller
+    {
+        #region Dependancy Injections
+        IBusinessLayer<TbPayment> oClsTbPayment;
+        public PaymentController(IBusinessLayer<TbPayment> payment)
+        {
+            oClsTbPayment = payment;
+        }
+        #endregion
+
+       
+    }
+}
