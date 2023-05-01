8bde9eec512de1fb12ad9f012334c7f105a97c08
eihap <eihapinfo@gmail.com>
2023-05-01 10:41:06 +0300

    update configrations

---
+++ UnitTests/ClsUsersTests.cs
@@ -0,0 +1,14 @@
+﻿using Bl;
+using Domains;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTests
+{
+    public class ClsUsersTests
+    {
+    }
+}
